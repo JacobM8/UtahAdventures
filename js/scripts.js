494c1050fb929e22cb605eebb282abba22bc0501
Jake Morrison <iamjacobmorrison@gmail.com>
2020-06-12 23:21:19 -0600

    added backcountry page and coming soon page

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#trip1").mouseover(function(){
+        $("#trip1").css("background-color", "black");
+    });
+    $("#trip1").mouseout(function(){
+        $("#trip1").css("background-color", "transparent");
+    });
+});
