e25a3d8fc6f9914f1f5f6fb7b0be8b32822b6e8d
Jake Morrison <iamjacobmorrison@gmail.com>
2020-06-13 09:07:06 -0600

    made text appear when mouse hover over individual trips

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("#trip1").mouseover(function(){
-        $("#trip1").css("background-color", "black");
-    });
-    $("#trip1").mouseout(function(){
-        $("#trip1").css("background-color", "transparent");
-    });
-});
